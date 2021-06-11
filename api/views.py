ff68f7210ed6f51a7770d264a8b0555bb0dfebc0
Himanshu <himanshudhiman9313@gmail.com>
2021-06-11 20:51:06 +0530

    updated

--- api/views.py
+++ api/views.py
@@ -1,12 +1,15 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+import json
+
 # Create your views here.
 
 @csrf_exempt
 def getData(request):
     if(request.method == "POST"):
-        print(request.POST)
+        data = json.loads(request.body.decode())
+        print(data)
         return HttpResponse("OK")
 
 
